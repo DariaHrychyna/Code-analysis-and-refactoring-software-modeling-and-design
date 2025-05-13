077d76630a9a8f58b5d8f919f1a46a00e201c97c
DariaHrychyna <115724896+DariaHrychyna@users.noreply.github.com>
2025-05-13 15:37:07 +0300

    Delete Presentation directory

--- Presentation/Business/book.py
+++
@@ -1,9 +0,0 @@
-class Book:
-    def __init__(self, book_id: int, title: str, author: str, price: float):
-        self.id = book_id
-        self.title = title
-        self.author = author
-        self.price = price
-
-    def get_info(self) -> str:
-        return f"{self.title}, автор {self.author} - {self.price: .2f}"
